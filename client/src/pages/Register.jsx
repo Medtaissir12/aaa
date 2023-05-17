e0bd85aacdc7ba89cb7d9c5364c1769cc3cb26d1
Medtaissir12 <119588376+Medtaissir12@users.noreply.github.com>
2023-05-17 01:15:57 +0100

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
